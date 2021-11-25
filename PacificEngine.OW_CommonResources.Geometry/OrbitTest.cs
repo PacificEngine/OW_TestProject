5c289e0464855434e52a187afaf7c712cce0d017
JoJo <joseph.salomone@dealertrack.com>
2021-11-25 07:51:53 -0800

    Better test

--- PacificEngine.OW_CommonResources.Geometry/OrbitTest.cs
+++ PacificEngine.OW_CommonResources.Geometry/OrbitTest.cs
@@ -10,6 +10,7 @@ namespace PacificEngine.OW_CommonResources.Geometry
     [TestFixture]
     public class OrbitTest
     {
+        public const float ERROR_PERCENTAGE = 0.001f;
 
         public static IEnumerable<TestCaseData> toKeplerCoordinatesTestCases
         {
@@ -20,21 +21,21 @@ namespace PacificEngine.OW_CommonResources.Geometry
                 var timberHearthMass = 3000000f;
                 var brittleHollowMass = 3000000f;
                 var giantDeepMass = 21780000f;
-                yield return new TestCaseData((float)0.001, (float)sunMass, 2f, 0f, new Vector3(0.0f, 0.0f, -2296.0f), new Vector3(417.4f, 0.0f, 0.0f), new Orbit.KeplerCoordinates(0f, 200, 0, 0, 0, 0)).SetName("toKeplerCoordinates_SunStationExample");
-                yield return new TestCaseData((float)0.001, (float)sunMass, 2f, 0f, new Vector3(-2867.9f, 0.0f, 4095.8f), new Vector3(-231.7f, 0.0f, -162.2f), new Orbit.KeplerCoordinates(0f, 200, 0, 0, 0, 0)).SetName("toKeplerCoordinates_HourglassTwinsExample");
-                yield return new TestCaseData((float)0.001, (float)hourglassMass, 1f, 0f, new Vector3(204.8f, 0.0f, 143.4f), new Vector3(16.2f, 0.0f, -23.2f), new Orbit.KeplerCoordinates(0f, 200, 0, 0, 0, 0)).SetName("toKeplerCoordinates_AshTwinExample");
-                yield return new TestCaseData((float)0.001, (float)hourglassMass, 1f, 0f, new Vector3(-204.8f, 0.0f, -143.4f), new Vector3(-16.2f, 0.0f, 23.2f), new Orbit.KeplerCoordinates(0f, 200, 0, 0, 0, 0)).SetName("toKeplerCoordinates_EmberTwinExample");
-                yield return new TestCaseData((float)0.001, (float)sunMass, 2f, 0f, new Vector3(1492.2f, 0.0f, -8462.5f), new Vector3(212.5f, 0.0f, 37.5f), new Orbit.KeplerCoordinates(0f, 200, 0, 0, 0, 0)).SetName("toKeplerCoordinates_TimberHearthExample");
-                yield return new TestCaseData((float)0.001, (float)timberHearthMass, 1f, 0f, new Vector3(886.3f, 0.0f, 156.3f), new Vector3(-9.5f, 0.0f, 53.9f), new Orbit.KeplerCoordinates(0f, 200, 0, 0, 0, 0)).SetName("toKeplerCoordinates_AttlerockExample");
-                yield return new TestCaseData((float)0.001, (float)timberHearthMass, 1f, 0f, new Vector3(-344.8f, 0.0f, -60.8f), new Vector3(9.5f, 0.0f, -53.9f), new Orbit.KeplerCoordinates(0f, 200, 0, 0, 0, 0)).SetName("toKeplerCoordinates_TimberHearthProbeExample");
-                yield return new TestCaseData((float)0.001, (float)sunMass, 2f, 0f, new Vector3(11513.3f, 0.0f, -2030.1f), new Vector3(32.1f, 0.0f, 182.2f), new Orbit.KeplerCoordinates(0f, 200, 0, 0, 0, 0)).SetName("toKeplerCoordinates_BrittleHollowExample");
-                yield return new TestCaseData((float)0.001, (float)brittleHollowMass, 1f, 0f, new Vector3(984.8f, 0.0f, -173.6f), new Vector3(9.5f, 0.0f, 53.9f), new Orbit.KeplerCoordinates(0f, 200, 0, 0, 0, 0)).SetName("toKeplerCoordinates_HollowLanternExample");
-                yield return new TestCaseData((float)0.001, (float)sunMass, 2f, 0f, new Vector3(3421.7f, 0.0f, -16098.0f), new Vector3(152.5f, 0.0f, 32.4f), new Orbit.KeplerCoordinates(0f, 200, 0, 0, 0, 0)).SetName("toKeplerCoordinates_GiantsDeepExample");
-                yield return new TestCaseData((float)0.001, (float)giantDeepMass, 1f, 0f, new Vector3(-1006.4f, 0.0f, 653.6f), new Vector3(80.4f, 0.0f, 123.8f), new Orbit.KeplerCoordinates(0f, 200, 0, 0, 0, 0)).SetName("toKeplerCoordinates_ProbeCannonExample");
-                yield return new TestCaseData((float)0.001, (float)sunMass, 2f, 0f, new Vector3(-3473.0f, 0.0f, 19696.2f), new Vector3(-139.3f, 0.0f, -24.6f), new Orbit.KeplerCoordinates(0f, 200, 0, 0, 0, 0)).SetName("toKeplerCoordinates_DarkBrambleExample");
-                yield return new TestCaseData((float)0.001, (float)sunMass, 2f, 0f, new Vector3(-24100.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 54.8f), new Orbit.KeplerCoordinates(0.82f, 200, 0, 0, 0, 0)).SetName("toKeplerCoordinates_InterloperExample");
-                yield return new TestCaseData((float)0.001, (float)sunMass, 2f, 0f, new Vector3(41999.8f, 5001.7f, -22499.9f), new Vector3(-46.9f, 28.1f, 24.7f), new Orbit.KeplerCoordinates(0f, 200, 0, 0, 0, 0)).SetName("toKeplerCoordinates_BackerSatiliteExample");
-                yield return new TestCaseData((float)0.001, (float)sunMass, 2f, 0f, new Vector3(24732.5f, -6729.5f, 4361.0f), new Vector3(31.6f, 119.8f, 5.6f), new Orbit.KeplerCoordinates(0f, 200, 0, 0, 0, 0)).SetName("toKeplerCoordinates_MapSatiliteExample");
+                yield return new TestCaseData((float)0.001, (float)sunMass, 2f, 0f, new Vector3(0.0f, 0.0f, -2296.0f), new Vector3(417.4f, 0.0f, 0.0f), new Orbit.KeplerCoordinates(0, 2296, 90f, 90f, 0, 17.25547f)).SetName("toKeplerCoordinates_SunStationExample");
+                yield return new TestCaseData((float)0.001, (float)sunMass, 2f, 0f, new Vector3(-2867.9f, 0.0f, 4095.8f), new Vector3(-231.7f, 0.0f, -162.2f), new Orbit.KeplerCoordinates(0, 5000.001f, 90, 305.2348f, 0, 55.46355f)).SetName("toKeplerCoordinates_HourglassTwinsExample");
+                yield return new TestCaseData((float)0.001, (float)hourglassMass, 1f, 0f, new Vector3(204.8f, 0.0f, 143.4f), new Vector3(16.2f, 0.0f, -23.2f), new Orbit.KeplerCoordinates(0, 250, 90, 233.8092f, 180, 41.86327f)).SetName("toKeplerCoordinates_AshTwinExample");
+                yield return new TestCaseData((float)0.001, (float)hourglassMass, 1f, 0f, new Vector3(-204.8f, 0.0f, -143.4f), new Vector3(-16.2f, 0.0f, 23.2f), new Orbit.KeplerCoordinates(0, 250, 90, 53.80923f, 180, 41.86327f)).SetName("toKeplerCoordinates_EmberTwinExample");
+                yield return new TestCaseData((float)0.001, (float)sunMass, 2f, 0f, new Vector3(1492.2f, 0.0f, -8462.5f), new Vector3(212.5f, 0.0f, 37.5f), new Orbit.KeplerCoordinates(0, 8593.086f, 90, 95.02123f, 0, 125.1692f)).SetName("toKeplerCoordinates_TimberHearthExample");
+                yield return new TestCaseData((float)0.001, (float)timberHearthMass, 1f, 0f, new Vector3(-344.8f, 0.0f, -60.8f), new Vector3(9.5f, 0.0f, -53.9f), new Orbit.KeplerCoordinates(0, 350.1f, 90, 72.95632f, 0, 13.03968f)).SetName("toKeplerCoordinates_TimberHearthProbeExample");
+                yield return new TestCaseData((float)0.001, (float)timberHearthMass, 1f, 0f, new Vector3(886.3f, 0.0f, 156.3f), new Vector3(-9.5f, 0.0f, 53.9f), new Orbit.KeplerCoordinates(0, 900.0001f, 90, 265.2352f, 0, 29.998f)).SetName("toKeplerCoordinates_AttlerockExample");
+                yield return new TestCaseData((float)0.001, (float)sunMass, 2f, 0f, new Vector3(11513.3f, 0.0f, -2030.1f), new Vector3(32.1f, 0.0f, 182.2f), new Orbit.KeplerCoordinates(0, 11690.89f, 90, 170.0452f, 0, 198.5069f)).SetName("toKeplerCoordinates_BrittleHollowExample");
+                yield return new TestCaseData((float)0.001, (float)brittleHollowMass, 1f, 0f, new Vector3(984.8f, 0.0f, -173.6f), new Vector3(9.5f, 0.0f, 53.9f), new Orbit.KeplerCoordinates(0, 999.227661f, 90, 122.3145f, 0, 72.51768f)).SetName("toKeplerCoordinates_HollowLanternExample");
+                yield return new TestCaseData((float)0.001, (float)sunMass, 2f, 0f, new Vector3(3421.7f, 0.0f, -16098.0f), new Vector3(152.5f, 0.0f, 32.4f), new Orbit.KeplerCoordinates(0, 16457.59f, 90, 101.9912f, 0, 331.6218f)).SetName("toKeplerCoordinates_GiantsDeepExample");
+                yield return new TestCaseData((float)0.001, (float)giantDeepMass, 1f, 0f, new Vector3(-1006.4f, 0.0f, 653.6f), new Vector3(80.4f, 0.0f, 123.8f), new Orbit.KeplerCoordinates(0, 1200, 90, 351.1121f, 180, 5.943623f)).SetName("toKeplerCoordinates_ProbeCannonExample");
+                yield return new TestCaseData((float)0.001, (float)sunMass, 2f, 0f, new Vector3(-3473.0f, 0.0f, 19696.2f), new Vector3(-139.3f, 0.0f, -24.6f), new Orbit.KeplerCoordinates(0, 20000, 90, 280.0383f, 0, 444.1865f)).SetName("toKeplerCoordinates_DarkBrambleExample");
+                yield return new TestCaseData((float)0.001, (float)sunMass, 2f, 0f, new Vector3(-24100.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 54.8f), new Orbit.KeplerCoordinates(0.8193752f, 13246.31f, 90, 180, 180, 239.4758f)).SetName("toKeplerCoordinates_InterloperExample");
+                yield return new TestCaseData((float)0.001, (float)sunMass, 2f, 0f, new Vector3(41999.8f, 5001.7f, -22499.9f), new Vector3(-46.9f, 28.1f, 24.7f), new Orbit.KeplerCoordinates(0.8884588f, 30535.38f, 28.1183f, 81.81603f, 91.35296f, 1253.788f)).SetName("toKeplerCoordinates_BackerSatiliteExample");
+                yield return new TestCaseData((float)0.001, (float)sunMass, 2f, 0f, new Vector3(24732.5f, -6729.5f, 4361.0f), new Vector3(31.6f, 119.8f, 5.6f), new Orbit.KeplerCoordinates(0, 26000, 10, 251.3132f, 270, 672.0341f)).SetName("toKeplerCoordinates_MapSatiliteExample");
             }
         }
 
@@ -66,24 +67,30 @@ namespace PacificEngine.OW_CommonResources.Geometry
         {
             var errorMessage = "Does not match Expected (" + expected.Item1.x + ", " + expected.Item1.y + ", " + expected.Item1.z + ")(" + expected.Item2.x + ", " + expected.Item2.y + ", " + expected.Item2.z + ") Actual (" + actual.Item1.x + ", " + actual.Item1.y + ", " + actual.Item1.z + ")(" + actual.Item2.x + ", " + actual.Item2.y + ", " + actual.Item2.z + ")";
 
-            Assert.AreEqual(expected.Item1.x, actual.Item1.x, Math.Min(expected.Item1.x, actual.Item1.x) * 0.0001f, errorMessage);
-            Assert.AreEqual(expected.Item1.y, actual.Item1.y, Math.Min(expected.Item1.y, actual.Item1.y) * 0.0001f, errorMessage);
-            Assert.AreEqual(expected.Item1.z, actual.Item1.z, Math.Min(expected.Item1.z, actual.Item1.z) * 0.0001f, errorMessage);
-            Assert.AreEqual(expected.Item2.x, actual.Item2.x, Math.Min(expected.Item2.x, actual.Item2.x) * 0.0001f, errorMessage);
-            Assert.AreEqual(expected.Item2.y, actual.Item2.y, Math.Min(expected.Item2.y, actual.Item2.y) * 0.0001f, errorMessage);
-            Assert.AreEqual(expected.Item2.z, actual.Item2.z, Math.Min(expected.Item2.z, actual.Item2.z) * 0.0001f, errorMessage);
+            Assert.AreEqual(expected.Item1.x, actual.Item1.x, Math.Max(expected.Item1.magnitude, 1f) * ERROR_PERCENTAGE, errorMessage);
+            Assert.AreEqual(expected.Item1.y, actual.Item1.y, Math.Max(expected.Item1.magnitude, 1f) * ERROR_PERCENTAGE, errorMessage);
+            Assert.AreEqual(expected.Item1.z, actual.Item1.z, Math.Max(expected.Item1.magnitude, 1f) * ERROR_PERCENTAGE, errorMessage);
+            Assert.AreEqual(expected.Item2.x, actual.Item2.x, Math.Max(expected.Item2.magnitude, 1f) * ERROR_PERCENTAGE, errorMessage);
+            Assert.AreEqual(expected.Item2.y, actual.Item2.y, Math.Max(expected.Item2.magnitude, 1f) * ERROR_PERCENTAGE, errorMessage);
+            Assert.AreEqual(expected.Item2.z, actual.Item2.z, Math.Max(expected.Item2.magnitude, 1f) * ERROR_PERCENTAGE, errorMessage);
         }
 
         private void compare(Orbit.KeplerCoordinates expected, Orbit.KeplerCoordinates actual)
         {
             var errorMessage = "Does not match Expected " + expected + " Actual + " + actual;
 
-            Assert.AreEqual(expected.eccentricity, actual.eccentricity, Math.Max(expected.eccentricity, 1f) * 0.0001f, errorMessage);
-            Assert.AreEqual(expected.semiMajorRadius, actual.semiMajorRadius, Math.Max(expected.semiMajorRadius, 1f) * 0.0001f, errorMessage);
-            Assert.AreEqual(expected.inclinationAngle, actual.inclinationAngle, Math.Max(expected.inclinationAngle, 1f) * 0.0001f, errorMessage);
-            Assert.AreEqual(expected.periapseAngle, actual.periapseAngle, Math.Max(expected.periapseAngle, 1f) * 0.0001f, errorMessage);
-            Assert.AreEqual(expected.ascendingAngle, actual.ascendingAngle, Math.Max(expected.ascendingAngle, 1f) * 0.0001f, errorMessage);
-            Assert.AreEqual(expected.timeSincePeriapse, actual.timeSincePeriapse, Math.Max(expected.timeSincePeriapse, 1f) * 0.0001f, errorMessage);
+            Assert.AreEqual(expected.eccentricity, actual.eccentricity, Math.Max(expected.eccentricity, 1f) * ERROR_PERCENTAGE, errorMessage);
+            Assert.AreEqual(expected.semiMajorRadius, actual.semiMajorRadius, Math.Max(expected.semiMajorRadius, 1f) * ERROR_PERCENTAGE, errorMessage);
+            Assert.AreEqual(expected.inclinationAngle, actual.inclinationAngle, Math.Max(expected.inclinationAngle, 1f) * ERROR_PERCENTAGE, errorMessage);
+            if (expected.eccentricity > ERROR_PERCENTAGE)
+            {
+                Assert.AreEqual(expected.periapseAngle, actual.periapseAngle, Math.Max(expected.periapseAngle, 1f) * ERROR_PERCENTAGE, errorMessage);
+            }
+            Assert.AreEqual(expected.ascendingAngle, actual.ascendingAngle, Math.Max(expected.ascendingAngle, 1f) * ERROR_PERCENTAGE, errorMessage);
+            if (expected.eccentricity > ERROR_PERCENTAGE)
+            {
+                Assert.AreEqual(expected.timeSincePeriapse, actual.timeSincePeriapse, Math.Max(expected.timeSincePeriapse, 1f) * ERROR_PERCENTAGE, errorMessage);
+            }
         }
     }
 }
